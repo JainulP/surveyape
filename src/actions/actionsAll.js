cbe57e928c07dabd78a3501b8c8f6222499556ed
JainulP <jainul.patel@sjsu.edu>
2018-05-07 00:46:35 -0700

    removing duplicated files

--- src/actions/actionsAll.js
+++
@@ -1,9 +0,0 @@
-export const GET_HOTELS = 'GET_HOTELS';
-
-export function GetHotels(obj) {
-    console.log("Get Hotels Loaded");
-    return {
-        type : "GET_HOTELS",
-        obj                                // this is same as newItem : newItem in ES6
-    }
-}
--- src/reducers/reducersAll.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import AllReducer from './reducer-common';
-
-const allReducers = combineReducers({
-    //all : AllReducer
-});
-
-export default allReducers;
